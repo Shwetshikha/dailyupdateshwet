5fee35861b859ca9f57d1191a8ffbd141a22b077
Shwet Shikha <Shwet@shorthillstech.com>
2022-07-29 13:04:48 +0530

    started javascript basics

---
+++ assignment/javascript.js
@@ -0,0 +1,26 @@
+var num1 = 34;
+var num2 = 24;
+console.log(num1 * num2 );
+
+var num1 = 677;
+var num2 = 78.99;
+var str1 = "This is a string"
+var str2 = "This is also a string"
+ // objects
+ var marks = {
+    manvi : 24,
+    shwet : 24,
+    shubham : 24
+ }
+ console.log(marks);
+
+ // Booleans
+ var a = true;
+ var b = false;
+ console.log(a, b)
+
+ var und = undefined;
+ console.log(und);
+
+var n = null;
+console.log(n);
