53ddf27ecfb3de71f62bf8d31fdaa4eec10462b2
Shwet Shikha <Shwet@shorthillstech.com>
2022-08-17 18:57:40 +0530

    Day 1 javascript started

---
+++ assignment/Javascript/javaday1.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+//declaring a variable and printing it//
+let name = 'SHWET';
+console.log(name) ;
+let firstname = 'Shwet'
+let lastname = 'Shikha'
+console.log(firstname);
+console.log(lastname);
+//constant//
+const intrestRate = 0.3;
+
+console.log(intrestRate);
